45eb8cfe7cfd1dc090f146d8ae246821b27cc1e7
Dromaf <pdvxai@gmail.com>
2019-10-20 21:51:22 +0300

    Updates

---
+++ precache-manifest.fa799a76c1605f3f4e49a064ec0bf034.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f833701fa4dca624a021bafc909ffc19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "582c17b9593a52e041c3",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "31e0671fee7dbf4622f1",
+    "url": "/static/css/main.272a91b6.chunk.css"
+  },
+  {
+    "revision": "582c17b9593a52e041c3",
+    "url": "/static/js/2.771b1ea6.chunk.js"
+  },
+  {
+    "revision": "31e0671fee7dbf4622f1",
+    "url": "/static/js/main.e6a9c055.chunk.js"
+  },
+  {
+    "revision": "76c41e34f22fe3986432",
+    "url": "/static/js/runtime~main.56ed8863.js"
+  },
+  {
+    "revision": "228b77a0f4a92aaced1068692d561368",
+    "url": "/static/media/bg-fon.228b77a0.jpg"
+  },
+  {
+    "revision": "ad8435df043a9b91f6700abcbd0e4d22",
+    "url": "/static/media/filmbg.ad8435df.jpg"
+  }
+]);
