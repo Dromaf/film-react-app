15896ed687bd3ccbfc9907f13ec4b95c23ae752d
Dromaf <pdvxai@gmail.com>
2019-10-20 22:06:39 +0300

    Updates

---
+++ precache-manifest.b1302f8f7d41bfe89155bcd4458cdbc5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2688938c465ddf1f533d750d459514a9",
+    "url": "/film-react-app/index.html"
+  },
+  {
+    "revision": "582c17b9593a52e041c3",
+    "url": "/film-react-app/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "f2f7a4b41458fc5ecb76",
+    "url": "/film-react-app/static/css/main.07841be6.chunk.css"
+  },
+  {
+    "revision": "582c17b9593a52e041c3",
+    "url": "/film-react-app/static/js/2.771b1ea6.chunk.js"
+  },
+  {
+    "revision": "f2f7a4b41458fc5ecb76",
+    "url": "/film-react-app/static/js/main.17569dfe.chunk.js"
+  },
+  {
+    "revision": "3638e4bbdbc86d21146c",
+    "url": "/film-react-app/static/js/runtime~main.c99dd5bd.js"
+  },
+  {
+    "revision": "228b77a0f4a92aaced1068692d561368",
+    "url": "/film-react-app/static/media/bg-fon.228b77a0.jpg"
+  },
+  {
+    "revision": "ad8435df043a9b91f6700abcbd0e4d22",
+    "url": "/film-react-app/static/media/filmbg.ad8435df.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fa799a76c1605f3f4e49a064ec0bf034.js"
+  "/film-react-app/precache-manifest.b1302f8f7d41bfe89155bcd4458cdbc5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/film-react-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
